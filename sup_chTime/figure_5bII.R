4974b37e51e38992db45d14ac398893a412c787f
lucasfr <lucas.franca.14@ucl.ac.uk>
2017-10-09 16:58:20 +0100

    Testing git-lfs

---
+++ sup_chTime/figure_5bII.R
@@ -0,0 +1,80 @@
+# Authors: Lucas França(1), Yujiang Wang(1,2,3)
+
+# 1 Department of Clinical and Experimental Epilepsy, UCL Institute of Neurology,
+University College London, London, United Kingdom
+
+# 2 Interdisciplinary Computing and Complex BioSystems (ICOS) research group,
+# School of Computing Science, Newcastle University, Newcastle upon Tyne,
+# United Kingdom
+
+# 3 Institute of Neuroscience, Newcastle University, Newcastle upon Tyne,
+United Kingdom
+
+# email address: lucas.franca.14@ucl.ac.uk, Yujiang.Wang@newcastle.ac.uk
+Website: https://lucasfr.github.io/, http://xaphire.de/
+
+rm(list = ls())
+
+library(ggplot2)
+library(RColorBrewer)
+library(extrafont)
+
+# IF THIS IS THE FIRST TIME YOU USE THE PACKAGE extrafont, PLEASE RUN
+# THE FOLLOWING COMMANDS
+
+#font_import()
+#loadfonts(device="win")       #Register fonts for Windows bitmap output
+#fonts()
+#loadfonts()
+
+
+swSpotDataFrame <- dget("swSpotDataFrame.R")
+
+swSpot <- swSpotDataFrame("effSize_I001_P005_D01_ch_1.mat","1")
+
+temp <-swSpotDataFrame("effSize_iI001_P005_D01_ch_1.mat","1")
+swSpot <- rbind(swSpot, temp)
+temp <-swSpotDataFrame("effSize_i2I001_P005_D01_ch_1.mat","1")
+swSpot <- rbind(swSpot, temp)
+
+
+
+temp <- swSpotDataFrame(paste("effSize_I001_P005_D01_ch_2.mat",
+                              sep=""),"2")
+swSpot <- rbind(swSpot, temp)
+temp <-swSpotDataFrame(paste("effSize_iI001_P005_D01_ch_2.mat",
+                             sep=""),"2")
+swSpot <- rbind(swSpot, temp)
+temp <-swSpotDataFrame(paste("effSize_i2I001_P005_D01_ch_2.mat",
+                             sep=""),"2")
+swSpot <- rbind(swSpot, temp)
+
+
+temp <- swSpotDataFrame(paste("effSize_I001_P005_D01_ch_3.mat",
+                              sep=""),"3")
+swSpot <- rbind(swSpot, temp)
+temp <-swSpotDataFrame(paste("effSize_iI001_P005_D01_ch_3.mat",
+                             sep=""),"3")
+swSpot <- rbind(swSpot, temp)
+temp <-swSpotDataFrame(paste("effSize_i2I001_P005_D01_ch_3.mat",
+                             sep=""),"3")
+swSpot <- rbind(swSpot, temp)
+
+
+ggplot(data = swSpot, aes(x = timeAnal, y = values, color = patName)) +
+  geom_point(data = swSpot, aes(shape = as.factor(sampRate)), size = 3) +
+  scale_shape_manual(values=1:nlevels(as.factor(swSpot$sampRate))) +
+  ylim(c(-0.5,2.5)) + xlim(c(0, 4.5)) + theme_bw(base_size = 20) +
+  theme(text=element_text(family="Times New Roman")) +
+  theme(panel.border = element_rect(size = 2)) +
+  theme(legend.background = element_rect(fill="white",
+                                         size=1, linetype="solid",
+                                         colour ="black")) +
+  geom_smooth(se=FALSE, size = 2) +
+  scale_color_manual(values = c("#ca0020", "#92c5de", "#0571b0")) +
+  xlab('Time (s)') + ylab('Effect Size') +
+  labs(color = "Channel") + labs(shape = "Sampling Rate (Hz)") +
+  theme(legend.title = element_text(size = 16),
+        legend.text = element_text(size = 14))
+
+ggsave(filename = "effSize.pdf", width = 8.86, height = 6.26, units = "in", useDingbats = F)
---
+++ sup_chTime/figure_5bSupIV.m
@@ -0,0 +1,114 @@
+% Authors: Lucas França(1), Yujiang Wang(1,2,3)
+
+% 1 Department of Clinical and Experimental Epilepsy, UCL Institute of Neurology,
+% University College London, London, United Kingdom
+
+% 2 Interdisciplinary Computing and Complex BioSystems (ICOS) research group,
+% School of Computing Science, Newcastle University, Newcastle upon Tyne,
+% United Kingdom
+
+% 3 Institute of Neuroscience, Newcastle University, Newcastle upon Tyne,
+% United Kingdom
+
+% email address: lucas.franca.14@ucl.ac.uk, Yujiang.Wang@newcastle.ac.uk
+% Website: https://lucasfr.github.io/, http://xaphire.de/
+
+
+clear
+close all
+addpath(genpath('../libs/'))
+
+%% DEFINING PARAMETERS
+
+id = 'Study 040';
+
+szPoint = 25220; szPointF = 25290.5; nullPoint = szPoint - 1800;
+nullPointF = szPointF - 1800; sampRate = 5000;
+
+
+%% iEEG.ORG SESSION (PLEASE REPLACE xxx WITH YOUR DETAILS)
+
+% session = IEEGSession(id,xxx,xxx);
+% sampRate = session.data.sampleRate;
+% 
+% segStart = nullPoint * sampRate;
+% segStop = nullPointF * sampRate;
+% values = session.data.getvalues(segStart:segStop,...
+%     1:length(session.data.channelLabels(:,1)));
+% 
+% segStart = szPoint * sampRate;
+% segStop = szPointF * sampRate;
+% valuesSz = session.data.getvalues(segStart:segStop,...
+%     1:length(session.data.channelLabels(:,1)));
+% 
+% save('figure_5bSupIV.mat','values','valuesSz')
+
+load('figure_5bSupIV.mat')
+
+%% LOOP OVER THE CHANNELS OF INTEREST (SOZ channels)
+
+for chNum = [1:4 43:45]
+
+    % SEIZURE SEGMENTS
+
+    szRaw{1} = valuesSz(:,chNum);
+
+    % CHANNELS ARE RESAMPLE ARE RESAMPLED IN ORDER TO EVALUATE INTERMEDIARY
+    % TIME SCALES
+
+    szRawi{1} = resample(valuesSz(:,chNum),6,5);
+    szRawi2{1} = resample(valuesSz(:,chNum),4,5);
+
+    % INTERICTAL SEGMENTS
+
+    nullRaw{1} = values(:,chNum);
+    nullRawi{1} = resample(values(:,chNum),6,5);
+    nullRawi2{1} = resample(values(:,chNum),4,5);
+
+    % NUMBER OF SEIZURES
+
+    szNum = 1:length(szRaw);
+
+    % WINDOW LENGTHS STUDIED
+
+    wSizes = [1024, 2048, 4096, 8192];
+
+    % SAMPLING RATES STUDIED
+
+    sampRates = [5000, 2500, 1000, 500, 250];
+
+    % EVALUATION OF TEH TIME SCALES FOR THE ORIGINAL SIGNAL
+
+    filename = ['effSize_' id '_ch_' num2str(chNum)];
+    swSpotEval(szRaw,nullRaw,filename,wSizes,szNum,...
+        5000,sampRates,-15,15,1,2,8,0.9);
+
+    % WINDOW LENGTHS STUDIED
+
+    wSizes = [1024, 2048, 4096, 8192];
+
+    % SAMPLING RATES STUDIED
+
+    sampRates = [3000,2000,1500,1200,750,600,300];
+
+    % EVALUATION OF TEH TIME SCALES FOR THE RESAMPLED SIGNAL
+
+    filename = ['effSize_i' id '_ch_' num2str(chNum)];
+    swSpotEval(szRawi,nullRawi,filename,wSizes,szNum,...
+        6000,sampRates,-15,15,1,2,8,0.9);
+
+    % WINDOW LENGTHS STUDIED
+
+    wSizes = [1024, 2048, 4096, 8192];
+
+    % SAMPLING RATES STUDIED
+
+    sampRates = [4000,800,400];
+
+    % EVALUATION OF TEH TIME SCALES FOR THE RESAMPLED SIGNAL
+
+    filename = ['effSize_i2' id '_ch_' num2str(chNum)];
+    swSpotEval(szRawi2,nullRawi2,filename,wSizes,szNum,...
+        4000,sampRates,-15,15,1,2,8,0.9);
+
+end
