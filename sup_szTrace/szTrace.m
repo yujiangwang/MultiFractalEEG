d81872156e491924174ea80c854d68485e48c8fb
lucasfr <lucas.franca.14@ucl.ac.uk>
2017-10-14 02:58:23 +0100

    More tests

--- sup_szTrace/szTrace.m
+++ sup_szTrace/szTrace.m
@@ -24,8 +24,9 @@ load('JR_12062011_1630_1730_ch1.mat');
 
 %% SETTING PARAMETERS
 
-tWindow = 2; segmentSize = 1024; qi=-15; qf=15; dq=1; Io=2; Np=8; Ra=0;
-sampRate = 512;
+segmentSize = 2048; qi=-15; qf=15; dq=1; Io=2; Np=8; Ra=0;
+sampRate = 512; tWindow = segmentSize/sampRate; szStart = 894773/512; 
+szStop = 907973/512;
 
 %% MULTIFRACTAL SPECTRA (NORMALISED PER EPOCH)
 
@@ -46,61 +47,81 @@ ll = llength(data,sampRate,tWindow);
 %% PLOTTING
 
 figure
-ttime = 3600/length(data):3600/length(data):length(data)/512;
+ttime = length(data)/sampRate/length(data):length(data)/sampRate/length(data):length(data)/sampRate;
 subplot(4,1,1)
 plot(ttime,data)
-xlim([3600/length(data) 3600])
-ylim([-500 500])
+hold on
+plot([szStart szStart],[min(data) max(data)],'Color',[202 0 32]./255, ...
+    'LineWidth',2)
+plot([szStop szStop],[min(data) max(data)],'Color', [202 0 32]./255, ...
+    'LineWidth',2)
+hold off
+xlim([700 1800])
+ylim([min(data) max(data)])
 xlabel('Time (s)')
 ylabel('\muV')
 title('EEG - channel 1')
 subplot(4,1,2)
-plot(2:2:2*length(chj.width(:,2)),chj.width(:,2))
+plot(tWindow:tWindow:tWindow*length(chj.width(:,2)),chj.width(:,2))
 hold on
-plot(2:2:2*length(smoothdata(chj.width(:,2),'movmedian',50)),...
+plot(tWindow:tWindow:tWindow*length(smoothdata(chj.width(:,2),'movmedian',50)),...
     smoothdata(chj.width(:,2),'movmedian',50),'Color',[0 0 0]./255,...
     'LineWidth',2)
+plot([szStart szStart],[min(chj.width(:,2)) max(chj.width(:,2))],'Color',...
+    [202 0 32]./255, 'LineWidth',2)
+plot([szStop szStop],[min(chj.width(:,2)) max(chj.width(:,2))],'Color',...
+    [202 0 32]./255, 'LineWidth',2)
 hold off
-xlim([2 3600])
-ylim([0.5 1.5])
+xlim([700 1800])
+ylim([min(chj.width(:,2)) max(chj.width(:,2))])
 xlabel('Time (s)')
 ylabel('\Delta\alpha')
 title('Multifractal spectra width')
 subplot(4,1,3)
-plot(2:2:2*length(dStat(:,2)),dStat(:,2))
+plot(tWindow:tWindow:tWindow*length(dStat(:,2)),dStat(:,2))
 hold on
-plot(2:2:2*length(smoothdata(dStat(:,2),'movmedian',50)),...
+plot(tWindow:tWindow:tWindow*length(smoothdata(dStat(:,2),'movmedian',50)),...
     smoothdata(dStat(:,2),'movmedian',50),'Color',[0 0 0]./255,...
     'LineWidth',2)
+plot([szStart szStart],[min(dStat(:,2)) max(dStat(:,2))],'Color',...
+    [202 0 32]./255, 'LineWidth',2)
+plot([szStop szStop],[min(dStat(:,2)) max(dStat(:,2))],'Color',...
+    [202 0 32]./255, 'LineWidth',2)
 hold off
-xlim([2 3600])
+xlim([700 1800])
+ylim([min(dStat(:,2)) max(dStat(:,2))])
 xlabel('Time (s)')
 ylabel('\sigma')
 title('St. Deviation')
 subplot(4,1,4)
-plot(2:2:2*length(ll),ll)
+plot(tWindow:tWindow:tWindow*length(ll),ll)
 hold on
-plot(2:2:2*length(smoothdata(ll,'movmedian',50)),...
+plot(tWindow:tWindow:tWindow*length(smoothdata(ll,'movmedian',50)),...
     smoothdata(ll,'movmedian',50),'Color',[0 0 0]./255,...
     'LineWidth',2)
+plot([szStart szStart],[min(ll) max(ll)],'Color',[202 0 32]./255, ...
+    'LineWidth',2)
+plot([szStop szStop],[min(ll) max(ll)],'Color', [202 0 32]./255, ...
+    'LineWidth',2)
 hold off
-xlim([2 3600])
+xlim([700 1800])
+ylim([min(ll) max(ll)])
 xlabel('Time (s)')
 ylabel('LL')
 title('Line Length')
 
 
-%% Yuj's hacking exploration
-dtime=2:2:2*length(chj.width(:,2));
-chts=chj.width(:,2);
-stdts=dStat(:,2);
-
-tid=dtime<1800 & dtime>800;
-figure(1)
-scatter(chts(tid),stdts(tid));
-lsline
-figure(2)
-ndhist([chts(tid),stdts(tid)])
-
-
-[c,p]=corr(chts(tid),stdts(tid))
+% %% Yuj's hacking exploration
+% dtime=2:2:2*length(chj.width(:,2));
+% chts=chj.width(:,2);
+% stdts=dStat(:,2);
+% 
+% tid=dtime<1800 & dtime>800;
+% figure(1)
+% scatter(chts(tid),stdts(tid));
+% lsline
+% figure(2)
+% ndhist([chts(tid),stdts(tid)])
+% 
+% 
+% [c,p]=corr(chts(tid),stdts(tid))
