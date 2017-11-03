aaac0d05fc0b439495254e43762492074c6697b7
lucasfr <lucas.franca.14@ucl.ac.uk>
2017-11-03 15:15:09 +0000

    Poster SfN 2017 - Washington, DC

--- libs/tweakedFBM.m
+++ libs/tweakedFBM.m
@@ -1,4 +1,4 @@
-% Authors: Lucas França(1), Yujiang Wang(1,2,3)
+% Authors: Lucas Franca(1), Yujiang Wang(1,2,3)
 
 % 1 Department of Clinical and Experimental Epilepsy, UCL Institute of Neurology,
 % University College London, London, United Kingdom
